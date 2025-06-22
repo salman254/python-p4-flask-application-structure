e9d4be5dec092e45e371e97d2b2210be81c12f1a
Salman <salmaanmohamed700@gmail.com>
2025-06-22 11:44:17 +0300

    Changes commit

--- server/app.py
+++ server/app.py
@@ -1,16 +1,33 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Welcome to my page!</h1>'
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
 
-@app.route('/<string:username>')
-def user(username):
-    return f'<h1>Profile for {username}</h1>'
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)
+    return param
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+@app.route('/count/<int:param>')
+def count(param):
+    return '\n'.join(str(i) for i in range(param)) + '\n'
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return 'Invalid operation', 400
+
+    return str(result)
